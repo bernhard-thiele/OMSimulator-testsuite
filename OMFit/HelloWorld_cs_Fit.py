2b6d53d18dea3266876fd99a3e3398ff5ca50b8c
Bernhard Thiele <bernhard.thiele@liu.se>
2018-02-20 21:24:54 +0100

    Added test for python parameter estimation bindings

---
+++ OMFit/HelloWorld_cs_Fit.py
@@ -0,0 +1,64 @@
+## status: correct
+## teardown_command: rm HelloWorld_cs_Fit_py.log
+
+from OMSimulator import OMSimulator
+from OMFit import OMFit
+import numpy as np
+
+session = OMSimulator()
+
+session.setLogFile("HelloWorld_cs_Fit_py.log")
+
+version = session.getVersion()
+
+model = session.newModel()
+session.setTempDirectory(".")
+session.setTolerance(model, 1e-5)
+
+# instantiate FMU
+session.instantiateFMU(model, "../FMUs/HelloWorld_cs.fmu", "HelloWorld")
+
+# create fitmodel for model
+fitmodel = OMFit(model)
+# fitmodel.describe()
+
+# Data generated from simulating HelloWorld.mo for 1.0s with Euler and 0.1s step size
+kNumSeries = 1
+kNumObservations = 11
+data_time = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+inputvars = []
+measurementvars = ["HelloWorld.x"]
+data_x = np.array([1, 0.9, 0.8100000000000001, 0.7290000000000001, 0.6561, 0.5904900000000001, 0.5314410000000001, 0.4782969000000001, 0.43046721, 0.387420489, 0.3486784401])
+
+fitmodel.initialize(kNumSeries, data_time, inputvars, measurementvars)
+# fitmodel.describe()
+
+fitmodel.addParameter("HelloWorld.x_start", 0.5);
+fitmodel.addParameter("HelloWorld.a", -0.5);
+fitmodel.addMeasurement(0, "HelloWorld.x", data_x)
+# fitmodel.describe()
+
+fitmodel.setOptions_max_num_iterations(25)
+fitmodel.solve("")
+
+status, state = fitmodel.getState()
+# print('status: {0}; state: {1}').format(OMFit.oms_status_str(status), OMFit.omsfit_fitmodelstate_str(state))
+
+status, startvalue1, estimatedvalue1 = fitmodel.getParameter("HelloWorld.a")
+status, startvalue2, estimatedvalue2 = fitmodel.getParameter("HelloWorld.x_start")
+# print('HelloWorld.a startvalue1: {0}; estimatedvalue1: {1}'.format(startvalue1, estimatedvalue1))
+# print('HelloWorld.x_start startvalue2: {0}; estimatedvalue2: {1}'.format(startvalue2, estimatedvalue2))
+is_OK1 = estimatedvalue1 > -1.1 and estimatedvalue1 < -0.9
+is_OK2 = estimatedvalue2 > 0.9 and estimatedvalue2 < 1.1
+print('HelloWorld.a estimation is OK: {0}'.format(is_OK1))
+print('HelloWorld.x_start estimation is OK: {0}'.format(is_OK2))
+
+# del fitmodel
+session.terminate(model)
+session.unload(model)
+
+## Result:
+## HelloWorld.a estimation is OK: True
+## HelloWorld.x_start estimation is OK: True
+## info:    Logging information has been saved to "HelloWorld_cs_Fit_py.log"
+## endResult
