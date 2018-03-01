b372ff01e8f87f8ab986a629efdb0ad0a9b39c5a
Lennart Ochel <lennart.ochel@liu.se>
2018-03-01 23:21:26 +0100

    Test new xml export

--- API/api-test02.lua
+++ API/api-test02.lua
@@ -1,5 +1,5 @@
 -- status: correct
--- teardown_command: rm -rf api-test02.log api-test02_tmp
+-- teardown_command: rm -rf api-test02.log api_test02.xml api-test02_tmp
 
 setLogFile("api-test02.log")
 oms2_setLoggingLevel(0)
@@ -22,8 +22,13 @@ oms2_addConnection("api_test02", "System1:s", "System2:s")
 oms2_addConnection("api_test02", "System1:v", "System2:v")
 oms2_addConnection("api_test02", "System1:a", "System2:a")
 
+status = oms2_saveModel("api_test02.xml", "api_test02")
+print("status code: " .. status)
+
 status = oms2_unloadModel("api_test02")
 print("status code: " .. status)
+
+os.execute("cat api_test02.xml")
 
 -- Result:
 -- status code: 0
@@ -31,5 +36,28 @@ print("status code: " .. status)
 -- status code: 0
 -- status code: 0
 -- status code: 0
+-- status code: 0
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <ssd:SystemStructureDescription xmlns:ssc="http://www.pmsf.net/xsd/SystemStructureCommonDraft" xmlns:ssd="http://www.pmsf.net/xsd/SystemStructureDescriptionDraft" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.pmsf.net/xsd/SystemStructureDescriptionDraft http://www.pmsf.net/xsd/SSP/Draft20170606/SystemStructureDescription.xsd">
+-- 	<FMICompositeModel Name="api_test02">
+-- 		<SubModel Name="System1" Type="FMU" ModelFile="../FMUs/DualMassOscillator.System1_me.fmu" />
+-- 		<SubModel Name="System2" Type="FMU" ModelFile="../FMUs/DualMassOscillator.System2_me.fmu" />
+-- 		<Connections>
+-- 			<Connection From="System1:F" To="System2:F">
+-- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
+-- 			</Connection>
+-- 			<Connection From="System1:s" To="System2:s">
+-- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
+-- 			</Connection>
+-- 			<Connection From="System1:v" To="System2:v">
+-- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
+-- 			</Connection>
+-- 			<Connection From="System1:a" To="System2:a">
+-- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
+-- 			</Connection>
+-- 		</Connections>
+-- 	</FMICompositeModel>
+-- 	<Experiment StartTime="0.000000" StopTime="1.000000" ResultFile="api_test02_res.mat" />
+-- </ssd:SystemStructureDescription>
 -- info:    Logging information has been saved to "api-test02.log"
 -- endResult
