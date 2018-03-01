1f388d74beec496288dd3537009236550638105c
Lennart Ochel <lennart.ochel@liu.se>
2018-03-01 16:39:21 +0100

    Add basic test cases for the API

---
+++ API/api-test01.lua
@@ -0,0 +1,44 @@
+-- status: correct
+-- teardown_command:
+
+oms2_setLoggingLevel(1)
+
+status = oms2_newFMIModel("api_test01")
+print("status code: " .. status)
+
+status = oms2_newFMIModel("api_test01")
+print("status code: " .. status)
+
+status = oms2_newFMIModel("test-01")
+print("status code: " .. status)
+
+status = oms2_newFMIModel("01test")
+print("status code: " .. status)
+
+status = oms2_unloadModel("01test")
+print("status code: " .. status)
+
+status = oms2_unloadModel("api_test01")
+print("status code: " .. status)
+
+status = oms2_unloadModel("api_test01")
+print("status code: " .. status)
+
+-- Result:
+-- status code: 0
+-- error:   A model called "api_test01" is already in the scope.
+-- status code: 3
+-- debug:   ComRef: invalid cref: test-01
+-- error:   "test-01" is not a valid model name.
+-- status code: 3
+-- debug:   ComRef: invalid cref: 01test
+-- error:   "01test" is not a valid model name.
+-- status code: 3
+-- debug:   ComRef: invalid cref: 01test
+-- error:   [oms2::Scope::unloadModel] There is no model called "01test" in the scope.
+-- status code: 3
+-- info:    Removed model from scope: api_test01
+-- status code: 0
+-- error:   [oms2::Scope::unloadModel] There is no model called "api_test01" in the scope.
+-- status code: 3
+-- endResult
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-.PHONY: all OMSimulator.log difftool
+.PHONY: all difftool
 
 all: difftool test
 
-test: OMSimulator.log OMFit.log
+test: API.log OMSimulator.log OMFit.log
 
 partest: difftool
 	cd partest && time ./runtests.pl -nocolour -with-xml
@@ -10,6 +10,10 @@ partest: difftool
 OMSimulator.log: difftool
 	$(MAKE) -C OMSimulator -f Makefile test > $@
 	grep == OMSimulator.log
+
+API.log: difftool
+	$(MAKE) -C API -f Makefile test > $@
+	grep == API.log
 
 OMFit.log: difftool
 	$(MAKE) -C OMFit -f Makefile test > $@
