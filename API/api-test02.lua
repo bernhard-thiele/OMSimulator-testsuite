0e14db2f3f498f325fd2a46c2f108399babe4f38
Lennart Ochel <lennart.ochel@liu.se>
2018-03-02 08:58:11 +0100

    Test xml import

--- API/api-test02.lua
+++ API/api-test02.lua
@@ -1,5 +1,5 @@
 -- status: correct
--- teardown_command: rm -rf api-test02.log api_test02.xml api-test02_tmp
+-- teardown_command: rm -rf api-test02.log api_test02.xml api_test02b.xml api-test02_tmp
 
 setLogFile("api-test02.log")
 oms2_setLoggingLevel(0)
@@ -17,10 +17,14 @@ status = oms2_instantiateFMU("api_test02", "../FMUs/DualMassOscillator.System2_m
 print("status code: " .. status)
 
 -- add connections
-oms2_addConnection("api_test02", "System1:F", "System2:F")
-oms2_addConnection("api_test02", "System1:s", "System2:s")
-oms2_addConnection("api_test02", "System1:v", "System2:v")
-oms2_addConnection("api_test02", "System1:a", "System2:a")
+status = oms2_addConnection("api_test02", "System1:F", "System2:F")
+print("status code: " .. status)
+status = oms2_addConnection("api_test02", "System1:s", "System2:s")
+print("status code: " .. status)
+status = oms2_addConnection("api_test02", "System1:v", "System2:v")
+print("status code: " .. status)
+status = oms2_addConnection("api_test02", "System1:a", "System2:a")
+print("status code: " .. status)
 
 status = oms2_saveModel("api_test02.xml", "api_test02")
 print("status code: " .. status)
@@ -28,9 +32,39 @@ print("status code: " .. status)
 status = oms2_unloadModel("api_test02")
 print("status code: " .. status)
 
+status, ident = oms2_loadModel("api_test02.xml")
+print("status code: " .. status .. " (" .. ident .. ")")
+
+status = oms2_deleteConnection("api_test02", "System1:a", "System2:a")
+print("status code: " .. status)
+status = oms2_deleteConnection("api_test02", "System1:s", "System2:s")
+print("status code: " .. status)
+status = oms2_deleteConnection("api_test02", "System1:F", "System2:F")
+print("status code: " .. status)
+status = oms2_deleteConnection("api_test02", "System1:v", "System2:v")
+print("status code: " .. status)
+
+status = oms2_saveModel("api_test02b.xml", "api_test02")
+print("status code: " .. status)
+
+status = oms2_unloadModel("api_test02")
+print("status code: " .. status)
+
 os.execute("cat api_test02.xml")
+os.execute("cat api_test02b.xml")
 
 -- Result:
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0
+-- status code: 0 (api_test02)
 -- status code: 0
 -- status code: 0
 -- status code: 0
@@ -43,19 +77,20 @@ os.execute("cat api_test02.xml")
 -- 		<SubModel Name="System1" Type="FMU" ModelFile="../FMUs/DualMassOscillator.System1_me.fmu" />
 -- 		<SubModel Name="System2" Type="FMU" ModelFile="../FMUs/DualMassOscillator.System2_me.fmu" />
 -- 		<Connections>
--- 			<Connection From="System1:F" To="System2:F">
--- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
--- 			</Connection>
--- 			<Connection From="System1:s" To="System2:s">
--- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
--- 			</Connection>
--- 			<Connection From="System1:v" To="System2:v">
--- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
--- 			</Connection>
--- 			<Connection From="System1:a" To="System2:a">
--- 				<ssd:ConnectionGeometry pointsX="" pointsY="" />
--- 			</Connection>
+-- 			<Connection From="System1:F" To="System2:F" />
+-- 			<Connection From="System1:s" To="System2:s" />
+-- 			<Connection From="System1:v" To="System2:v" />
+-- 			<Connection From="System1:a" To="System2:a" />
 -- 		</Connections>
+-- 	</FMICompositeModel>
+-- 	<Experiment StartTime="0.000000" StopTime="1.000000" ResultFile="api_test02_res.mat" />
+-- </ssd:SystemStructureDescription>
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <ssd:SystemStructureDescription xmlns:ssc="http://www.pmsf.net/xsd/SystemStructureCommonDraft" xmlns:ssd="http://www.pmsf.net/xsd/SystemStructureDescriptionDraft" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.pmsf.net/xsd/SystemStructureDescriptionDraft http://www.pmsf.net/xsd/SSP/Draft20170606/SystemStructureDescription.xsd">
+-- 	<FMICompositeModel Name="api_test02">
+-- 		<SubModel Name="System1" Type="FMU" ModelFile="../FMUs/DualMassOscillator.System1_me.fmu" />
+-- 		<SubModel Name="System2" Type="FMU" ModelFile="../FMUs/DualMassOscillator.System2_me.fmu" />
+-- 		<Connections />
 -- 	</FMICompositeModel>
 -- 	<Experiment StartTime="0.000000" StopTime="1.000000" ResultFile="api_test02_res.mat" />
 -- </ssd:SystemStructureDescription>
